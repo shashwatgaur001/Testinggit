9124d1d86d4a5151a6d532f02b1ae32a4475284d
shashwatgaur001 <shashwatgaur001@gmail.com>
2023-04-01 12:47:21 +0530

    secondfile

---
+++ scr.js
@@ -0,0 +1,8 @@
+console.log(document.domain);
+console.log(document.all[1]);
+document.all[13].textContent="Hello";    
+console.log(document.all[13]);  
+console.log(document.links);
+let ht=document.getElementById('header-title');
+ht.textContent="hello"  
+console.log('No git')
